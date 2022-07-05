7e2452b3dd5533e8685e3f241edebe4e1f9550f1
Tureika Valeria <ler4ik65@gmail.com>
2022-07-05 08:59:55 +0200

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const sum = (a, b) => a + b;
+// eslint-disable-next-line no-underscore-dangle
+const function_ = () => {
+    // eslint-disable-next-line unicorn/consistent-function-scoping
+    const sum = (a, b) => a + b;
+    let x = 10;
+    let y = 20;
 
-let x = 10;
-let y = 20;
+    console.log(sum(x, y)); // 30
+    debugger;
+    x = 20;
+    y = 30;
 
-console.log(sum(x, y)); // 30
-debugger;
-x = 20;
-y = 30;
+    console.log(sum(x, y)); // 50
+};
 
-console.log(sum(x, y)); // 50
+function_();
